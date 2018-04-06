32d9880fd6f3e831bf0c25e01a259ff4ef6248d9
mskian <veerfun23@gmail.com>
2018-04-06 22:39:18 +0530

    enhancement

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
  *            Plugin Name: Firebase Email Subscription
  *            Plugin URI: https://www.allwebtuts.com/build-an-email-subscriber-list-by-using-firebase/
  *            Description: Build your Email List - Collect your Blog Visitor's Email on Firebase realtime Database
- *            Version: 1.0
+ *            Version: 1.1
  *            License: GPL-3.0+
  *            License URI: https://www.gnu.org/licenses/gpl-3.0.txt
  */
@@ -33,6 +33,7 @@ $email = htmlspecialchars($email,ENT_COMPAT);
     "name": "' . $name . '","email": "' . $email . '"}
         
 }';
+
     //replace https://example-project.firebaseio.com with Firebase Realtime DB URL - DON'T REMOVE /subscribers.json
     $url = "https://example-project.firebaseio.com/subscribers.json";
     $ch = curl_init();
@@ -56,7 +57,7 @@ $email = htmlspecialchars($email,ENT_COMPAT);
 }
 
 // Add your Own Message
-$message= '<div class="container"><div class="row"><div class="col-lg-6 col-lg-offset-3"><div class="alert alert-success text-center"><i class="fa fa-thumbs-up"></i> <b>Thanks for Subscribe to Our Blog Post  Updates</b></div></div></div></div>';
+$message= '<div class="container"><div class="row"><div class="col-lg-6 col-lg-offset-3 mx-auto"><div class="alert alert-success text-center"><i class="fas fa-thumbs-up"></i> <b>Thanks for Subscribe to Our Blog Post  Updates</b></div></div></div></div>';
 
 ?>
 
@@ -64,71 +65,62 @@ $message= '<div class="container"><div class="row"><div class="col-lg-6 col-lg-o
 <html lang="en">
 <head>
 
-<!-- meta character set -->
 <meta charset="utf-8">
-
-<!-- Always force latest IE rendering engine or request Chrome Frame -->
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-<!-- Mobile Specific Meta -->
-<meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <title>Subscribe to our Updates</title>
 <meta name="Description" content="Subscribe to our Blog Post Updates.">
-<?php $current_page = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
- echo '<link rel="canonical" href="'.$current_page.'" itemprop="url"/>'; ?>
-	
 
 <!-- CSS and Fonts -->
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href='https://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
 
 <style>
 body {
   background: #eee !important;
-  font-family: 'Lato', sans-serif;
-  
+  font-family: 'Exo 2', sans-serif;
 }
 .login-form{
-	margin:3% auto 0;
-	max-width:380px;
+  margin:3% auto 0;
+  max-width:380px;
 }
 .login-form h1{
-	font-size: 30pt;
-	font-weight: 700;
+  font-size: 30pt;
+  font-weight: 700;
     letter-spacing: -1px;
-    font-family: 'Lato', sans-serif;
+    font-family: 'Exo 2', sans-serif;
 }
 .form-header,.form-footer{
-	background-color: rgba(255, 255, 255, .8);
-  	border: 1px solid rgba(0,0,0,0.1);
+  background-color: rgba(255, 255, 255, .8);
+    border: 1px solid rgba(0,0,0,0.1);
 }
 .form-signin{
-	padding: 45px 35px 45px;
-  	background-color: #fff;
-  	border: 1px solid rgba(0,0,0,0.1);  
-  	border-bottom: 0px; 
-  	border-top: 0px;  
+  padding: 45px 35px 45px;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,0.1);  
+    border-bottom: 0px; 
+    border-top: 0px;  
 }
 .form-register{
-	padding: 45px 35px 45px;
-  	background-color: #fff;
-  	border: 1px solid rgba(0,0,0,0.1);  
-  	border-bottom: 0px; 
-  	border-top: 0px; 
+  padding: 45px 35px 45px;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,0.1);  
+    border-bottom: 0px; 
+    border-top: 0px; 
 }
 .form-header{
-	text-align: center;
-	padding: 15px 40px;
-	border-radius: 10px 10px 0 0;
+  text-align: center;
+  padding: 15px 40px;
+  border-radius: 10px 10px 0 0;
 }
 .form-header i{font-size:60px;}
 .form-footer {
-	padding: 15px 40px;	
+  padding: 15px 40px; 
 }
 .form-signin-heading{
-	margin-bottom: 30px;
+  margin-bottom: 30px;
 }
 .bt-login{
     background-color: #ff8627;
@@ -180,16 +172,34 @@ textarea.parsley-error:focus {
     opacity: 1;
     color: #a94442;
 }
+pre {
+  background-color: #f7f7f9;
+  border: 1px solid #e1e1e8;
+color: #336cac;
+font-size: 17px;
+}
+pre:before {
+    color: #333;
+}
+.btn {
+    margin-bottom:4px;white-space: normal;
+}
+.input {
+    margin-bottom:4px;white-space: normal;
+}
 </style>
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
 
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
+
 
 </head>
 <body>
@@ -199,18 +209,19 @@ textarea.parsley-error:focus {
 <div class="container">
 <div class="login-form">
 
-<h1 class="text-center">Free Email Updates</h1>
+<h2 class="text-center">Free Email Updates</h2>
+<br />
 
 <div class="form-header">
-<i class="fa fa-user"></i>
+<i class="fas fa-bell"></i>
 </div>
 
 <form class="form-signin" method="post">
 <div class="form-group">
-<input type="text" name="name" class="form-control" required data-parsley-trigger="keyup" placeholder="Your Name">
+<input type="text" name="name" class="form-control" data-parsley-required="true" data-parsley-error-message="Enter a your Name" placeholder="Your Name">
 </div>
 <div class="form-group">
-<input class="form-control" type="email" name="email" required data-parsley-type="email" data-parsley-trigger="keyup" placeholder="Email address">
+<input class="form-control" type="email" name="email" data-parsley-required="true" data-parsley-type="email" placeholder="Email address">
 </div>
 <button type="submit" class="btn btn-block bt-login">Subscribe</button>
 </form>
@@ -225,17 +236,14 @@ echo $message;
 }
 ?>
 
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.5.1/parsley.min.js"></script>
-
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.8.1/parsley.min.js" integrity="sha256-XqEmjxbIPXDk11mQpk9cpZxYT+8mRyVIkko8mQzX3y8=" crossorigin="anonymous"></script>
 
 <script>
 $(document).ready(function(){
-	$('form').parsley();
+  $('form').parsley();
 });
 </script>
-
 
 </body>
 </html>
